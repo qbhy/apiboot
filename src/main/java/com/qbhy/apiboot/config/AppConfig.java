97ca84f149c801825a851fc95f5358eb6d6ff488
qbhy <96qbhy@gmail.com>
2019-09-21 16:32:55 +0800

    fix

--- src/main/java/com/qbhy/apiboot/config/AppConfig.java
+++ src/main/java/com/qbhy/apiboot/config/AppConfig.java
@@ -1,8 +1,17 @@
 package com.qbhy.apiboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 @Configuration
 public class AppConfig {
 
+    @Bean(name = "applicationEventMulticaster")
+    public SimpleApplicationEventMulticaster simpleApplicationEventMulticaster() {
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return eventMulticaster;
+    }
 }
