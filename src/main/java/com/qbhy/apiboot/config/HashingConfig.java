e262fd26e2ae24129c0dfb2d0e2b1041a8ff42a1
qbhy <96qbhy@gmail.com>
2019-09-25 17:49:02 +0800

    添加hashing配置

--- src/main/java/com/qbhy/apiboot/config/HashingConfig.java
+++ src/main/java/com/qbhy/apiboot/config/HashingConfig.java
@@ -2,10 +2,12 @@ package com.qbhy.apiboot.config;
 
 import com.qbhy.apiboot.framework.contracts.hashing.SecretProvider;
 import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Data
+@ConfigurationProperties(prefix = "hashing")
 public class HashingConfig implements SecretProvider {
     private String secret = "apiboot";
 
